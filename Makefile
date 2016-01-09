761df9f62d9ca583643e8d166a29c96da2d5653d
Yao Han <verihy@gmail.com>
2016-01-10 04:09:36 +0800

    keygen

--- Makefile
+++ Makefile
@@ -1,23 +1,30 @@
 
 CXX = g++
-CXXFLAGS = -fPIC -std=c++11 -Wall -I/usr/local/include/pbc -I/usr/include/glib-2.0 \
+CXXFLAGS = -fPIC  -Wall -I/usr/local/include/pbc -I/usr/include/glib-2.0 \
 		   -I/usr/lib/x86_64-linux-gnu/glib-2.0/include 
 
 LDFLAGS = -lglib-2.0 \
 		  -lgmp \
 		  -lcrypto \
 		  -lpython2.7 \
-		  -lpbc 
+		  -lpbc \
+		  -Xlinker -export-dynamic -Wl,-O1 -Wl,-Bsymbolic-functions
+
+.PHONY:clean
+
 
 all: abe.so test
 
 
 
-abe.so: libbswabe.a abe.cc
-	$(CXX) -O2 abe.cc -o abe.so libbswabe.a -shared $(CXXFLAGS) $(LDFLAGS)
+abe.so: libbswabe.a abe.cc common.o
+	$(CXX) -o abe.so abe.cc common.o libbswabe.a $(CXXFLAGS) $(LDFLAGS) -shared
 
-test: libbswabe.a abe.cc
-	$(CXX) abe.cc libbswabe.a $(CXXFLAGS) $(LDFLAGS) -g -o test
+common.o: common.c common.h
+	$(CXX) -O1 -c common.c $(CXXFLAGS)
+
+test: libbswabe.a abe.cc common.o
+	$(CXX) abe.cc common.o libbswabe.a $(CXXFLAGS) $(LDFLAGS) -g -o test
 
 clean:
 	rm -rf *.out *.o *.so test
--- abe.cc
+++ abe.cc
@@ -8,67 +8,107 @@
 #include <bswabe.h>
 #include <cstdio>
 
+#include "cpabe.h"
+
+#include "private.h"
+
+#include <iostream>
+using std::cout;
+using std::cerr;
+using std::endl;
+
 int foo(int a, int b) {
     return a + b;
 }
+
+
+namespace {
+
+
+void do_free_prv(bswabe_prv_t *prv) {
+    element_clear(prv->d);
+    for (size_t i = 0; i < prv->comps->len; ++i) {
+        bswabe_prv_comp_t c;
+        c = g_array_index(prv->comps, bswabe_prv_comp_t,  i);
+        element_clear(c.d);
+        element_clear(c.dp);
+    }
+    g_array_free(prv->comps, 1);
+    free(prv);
+}
+
+}
+
+
 
 
 
 static PyObject* setup(PyObject *self, PyObject *args) {
     (void)self;
     (void)args;
+
     bswabe_pub_t *pub;
     bswabe_msk_t *msk;
 
     bswabe_setup(&pub, &msk);
     GByteArray *p = bswabe_pub_serialize(pub);
-    GByteArray *m = bswabe_msk_serialize(msk);
-    PyObject *ret = PyTuple_Pack(
+    GByteArray *m = bswabe_msk_serialize(msk); PyObject *ret = NULL;
+    ret = PyTuple_Pack(
             2,
-            PyByteArray_FromStringAndSize(reinterpret_cast<const char*>(p->data), p->len),
-            PyByteArray_FromStringAndSize(reinterpret_cast<const char*>(m->data), m->len)
+            PyString_FromStringAndSize(reinterpret_cast<const char*>(p->data), p->len),
+            PyString_FromStringAndSize(reinterpret_cast<const char*>(m->data), m->len)
             );
 
-    g_byte_array_free(p, 1);
     g_byte_array_free(m, 1);
-    free(pub);
-    free(msk);
+    g_byte_array_free(p, 1);
+    bswabe_msk_free(msk);
+    bswabe_pub_free(pub);
     return ret;
 }
 
-// TODO char* to GbyteArray*
-//GByteArray* eat
 
+// keygen(pub_path, msk_path, attrs)
 static PyObject* keygen(PyObject *self, PyObject *args) {
     (void)self;
+    
 
-    char *pk, *mk;
-    int pk_len, mk_len;
+    char *pub_path, *msk_path;
     PyObject *attr_tuple;
 
-    if (!PyArg_ParseTuple(args, "t#t#O",
-                &pk, &pk_len,
-                &mk, &mk_len,
-                &attr_tuple
-                )) {
-        return nullptr;
+    if (!PyArg_ParseTuple(args, "ssO", &pub_path, &msk_path, &attr_tuple) ) {
+        return NULL;
     }
-
-
-    //auto pub = bswabe_pub_unserialize(pk)
     
     std::vector<std::string> attrs;
-    auto attrlen = PyTuple_Size(attr_tuple);
+    int attrlen = PyTuple_Size(attr_tuple);
     for (int i = 0; i < attrlen; ++i) {
         attrs.push_back(PyString_AsString(PyTuple_GetItem(attr_tuple, i)));
     }
+    std::vector<char*> attributes;
+    for (int i = 0; i < attrlen; ++i) {
+        attributes.push_back(const_cast<char*>(attrs[i].data()));
+    }
+    attributes.push_back(NULL);
 
-    //pub = bswabe_pub_unserrialize();
 
-    PyMem_Free(pk);
-    PyMem_Free(mk);
-    PyMem_Free(attr_tuple);
-    return PyLong_FromLong(mk_len);
+    bswabe_pub_t *pub = bswabe_pub_unserialize(suck_file(pub_path), 1);
+    bswabe_msk_t *msk = bswabe_msk_unserialize(pub, suck_file(msk_path), 1);
+    bswabe_prv_t *prv = bswabe_keygen(pub, msk, attributes.data());
+
+    GByteArray *prv_gbyte = bswabe_prv_serialize(prv);
+
+    PyObject *ret = NULL;
+    std::string content(reinterpret_cast<char*>(prv_gbyte->data), prv_gbyte->len);
+    //ret = PyString_FromStringAndSize(reinterpret_cast<const char*>(prv_gbyte->data), prv_gbyte->len);
+    //ret = PyByteArray_FromStringAndSize(reinterpret_cast<const char*>(prv_gbyte->data), prv_gbyte->len);
+
+
+    g_byte_array_free(prv_gbyte, 1);
+    do_free_prv(prv);
+    bswabe_msk_free(msk);
+    bswabe_pub_free(pub);
+
+    return PyString_FromStringAndSize(content.data(), content.size());
 }
 
 static PyMethodDef AbeModuleMethods[] = {
@@ -98,7 +138,7 @@ PyMODINIT_FUNC initabe(void) {
 }
 
 int main() {
-    PyObject *tmp = setup(NULL, NULL);
-    (void)tmp;
+    //PyObject *tmp = setup(NULL, NULL);
+    //(void)tmp;
     return 0;
 }
